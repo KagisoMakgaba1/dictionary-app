04e547386533346c94eeae13a43671f69885b14e
kagisomakgaba <kagisomakgaba99@gmail.com>
2024-01-15 23:26:52 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        
-        <Dictionary />
-        
+      <div className="container mt-5">
+        <Dictionary />  
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,19 +3,17 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
     return (
-        <div className="Dictionary">
-            <div className="body-bg">
-            <section>
-                <h3 className="fs-5">Hoot! What word do you want to look up?</h3>
-            <form >
-                <input type="search" className="Search-Bar" placeholder="Type a word" autoFocus={true}  />
-                <br /><sub>Suggestions: galaxy, flower, rabbit, etc.</sub>
+      <div className="Dictionary mt-5">
+        <div className="body-bg">
+          <section>
+            <h3>Hoot! What word do you want to look up?</h3>
+            <form>
+                <input type="search" placeholder="Type a word"/>
+                <br /><sub>Suggestions: coding, love, galaxy, etc</sub>
             </form>
-            </section>
-            
-            <div className="App-footer text-center">👩‍💻 Coded by Kagiso Makgaba and is open-sourced on GitHub and is hosted on Netlify<br />
-      </div>
-            </div>
+          </section>
+          <footer>👩‍🏫 Coded by Kagiso Makgaba and open-sourced on Github</footer>
         </div>
-    )
+      </div>
+    );
 }
