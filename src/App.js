0ea743d8ff340774c3e88a3b2d3f2b6c7add6b1e
kagisomakgaba <kagisomakgaba99@gmail.com>
2024-01-16 21:02:55 +0200

    API Intergration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
 import Dictionary from "./Dictionary";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container mt-5">
-        <Dictionary />  
+        <header>
+          <h2 className="mt-5">dic·tion·ar·y</h2>
+          <hr />
+          <h3>Hoot! What word do you want to look up?</h3>
+        </header>
+        <main>
+          <Dictionary defaultKeyword="Programmer" />
+        </main>
+        <footer>
+          <small>
+            <p>
+              👩‍💻 Coded by Kagiso Makgaba and open-sourced on{" "}
+              <a
+                href="https://github.com/KagisoMakgaba1/dictionary-app"
+                className="App-link"
+                target="_blank"
+                rel="noreferrer noopener"
+                alt="Link to Dictionary App GitHub Repository"
+              >
+                Github
+              </a>
+              , and hosted on Netlify
+            </p>
+          </small>
+        </footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-    return (
-      <div className="Dictionary mt-5">
-        <div className="body-bg">
-          <section>
-            <h3>Hoot! What word do you want to look up?</h3>
-            <form>
-                <input type="search" placeholder="Type a word"/>
-                <br /><sub>Suggestions: coding, love, galaxy, etc</sub>
-            </form>
-          </section>
-          <footer>👩‍🏫 Coded by Kagiso Makgaba and open-sourced on Github</footer>
-        </div>
-      </div>
-    );
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
+
+  function handleResponse (response) {
+    setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  function search() {
+    // documentation: https://dictionaryapi.dev
+     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+     axios.get(apiUrl).then(handleResponse);
+
+     let pexelsApiKey =
+       "XwsNI1LGL6ZH5E0zkceJXb5WP19Huuv8iPehPjycCZkCyf4c7YROWQPd";
+       let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+       let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse)}
+
+
+function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+
+     function updateKeyword (event) {
+        setKeyword(event.target.value);
+    }
+
+    if (loaded) {
+      return (
+    <div className="Dictionary mt-5">
+      <section>
+        <form onSubmit={handleSubmit}>
+          <input type="search" placeholder="Type a word..." onChange={updateKeyword} defaultValue={props.defaultKeyword}/>
+          <br />
+          <sub>Suggested words: coding, love, galaxy, book...</sub>
+        </form>
+      </section>
+      
+      <Results results={results} />
+      <Photos photos={photos} />
+    </div>
+  );
+  } else {
+    load();
+    return "Loading..."
+  }
 }
